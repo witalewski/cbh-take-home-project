e82d5044c9b5bb540f6ca98496c7e5146393a60c
Adam Witalewski <witalewski@gmail.com>
2023-05-23 13:53:38 +0200

    refactor: return early when no input is provided to reduce number of checks later

--- dpk.js
+++ dpk.js
@@ -7,24 +7,23 @@ const calculateHash = (data) =>
   crypto.createHash("sha3-512").update(data).digest("hex");
 
 exports.deterministicPartitionKey = (event) => {
+  if (!event) {
+    return TRIVIAL_PARTITION_KEY;
+  }
+
   let candidate;
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = calculateHash(data);
-    }
+  if (event.partitionKey) {
+    candidate = event.partitionKey;
+  } else {
+    const data = JSON.stringify(event);
+    candidate = calculateHash(data);
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
+  if (typeof candidate !== "string") {
+    candidate = JSON.stringify(candidate);
   }
+
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
     candidate = calculateHash(candidate);
   }
